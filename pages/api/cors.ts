7ddd060595c24bd1d062b99aac6dd5afe48deb91
nicolascasmuz <nicolascasmuz@hotmail.com>
2024-12-19 09:56:16 -0300

    commit-004

--- pages/api/cors.ts
+++ pages/api/cors.ts
@@ -1,15 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import methods from "micro-method-router";
-import * as yup from "yup";
-import { authMiddleware } from "lib/middlewares";
-import { handler, updateData } from "lib/controllers/user";
-
-let emailSchema = yup.object({
-  email: yup.string().required(),
-});
-
-///
-
 import Cors from "cors";
 
 // Initializing the cors middleware
@@ -44,21 +33,5 @@ export default async function corsHandler(
   await runMiddleware(req, res, cors);
 
   // Rest of the API logic
-  methods({
-    async get(req: NextApiRequest, res: NextApiResponse) {
-      const { userData } = await authMiddleware(req, res, handler);
-      res.status(200).json(userData);
-    },
-    async patch(req: NextApiRequest, res: NextApiResponse) {
-      try {
-        await emailSchema.validate(req.body);
-      } catch (e) {
-        res.status(422).send({ field: "email", message: e });
-      }
-
-      const newData = await updateData(req.body);
-
-      res.status(200).json(newData.userData);
-    },
-  });
+  res.json({ message: "Hello Everyone!" });
 }
