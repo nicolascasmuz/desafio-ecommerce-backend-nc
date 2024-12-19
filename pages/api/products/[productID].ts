f5500f4395b539eca5d598ff977ee7042c383ee4
nicolascasmuz <nicolascasmuz@hotmail.com>
2024-12-19 10:25:06 -0300

    commit-007

--- pages/api/products/[productID].ts
+++ pages/api/products/[productID].ts
@@ -7,21 +7,43 @@ let querySchema = yup.object({
   productID: yup.string().required(),
 });
 
-export default methods({
-  async get(req: NextApiRequest, res: NextApiResponse) {
-    try {
-      await querySchema.validate(req.query);
-    } catch (e) {
-      res.status(422).send({ field: "query", message: e });
-    }
+// Configuración del middleware CORS
+function setCorsHeaders(res: NextApiResponse) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Permitir solicitudes de cualquier origen
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"); // Métodos permitidos
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Encabezados permitidos
+}
 
-    const { productID } = req.query;
+export default async function handlerWithCors(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  setCorsHeaders(res);
 
-    try {
-      const foundProduct = await productsIndex.getObject(productID.toString());
-      res.status(200).json(foundProduct);
-    } catch (error) {
-      res.status(400).json({ message: "product not found" });
-    }
-  },
-});
+  // Manejo de preflight requests (solicitudes OPTIONS)
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+
+  await methods({
+    async get(req: NextApiRequest, res: NextApiResponse) {
+      try {
+        await querySchema.validate(req.query);
+      } catch (e) {
+        res.status(422).send({ field: "query", message: e });
+      }
+
+      const { productID } = req.query;
+
+      try {
+        const foundProduct = await productsIndex.getObject(
+          productID.toString()
+        );
+        res.status(200).json(foundProduct);
+      } catch (error) {
+        res.status(400).json({ message: "product not found" });
+      }
+    },
+  })(req, res);
+}
