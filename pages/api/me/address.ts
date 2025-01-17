a1f16d8da58642b8b7127795545425a1cf1c2950
Nicolás Casmuz <nicolascasmuz@gmail.com>
2025-01-17 00:50:43 -0300

    commit-020

--- pages/api/me/address.ts
+++ pages/api/me/address.ts
@@ -2,21 +2,49 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import methods from "micro-method-router";
 import * as yup from "yup";
 import { updateAddress } from "lib/controllers/user";
+import Cors from "cors";
 
 let emailSchema = yup.object({
   email: yup.string().required(),
 });
 
-export default methods({
-  async patch(req: NextApiRequest, res: NextApiResponse) {
-    try {
-      await emailSchema.validate(req.body);
-    } catch (e) {
-      res.status(422).send({ field: "email", message: e });
-    }
-
-    const newData = await updateAddress(req.body);
-
-    res.status(200).json(newData.address);
-  },
+const cors = Cors({
+  methods: ["GET", "POST", "PATCH", "OPTIONS", "HEAD"],
 });
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function corsHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await runMiddleware(req, res, cors);
+
+  await methods({
+    async patch(req: NextApiRequest, res: NextApiResponse) {
+      try {
+        await emailSchema.validate(req.body);
+      } catch (e) {
+        res.status(422).send({ field: "email", message: e });
+      }
+
+      const newData = await updateAddress(req.body);
+
+      res.status(200).json(newData.address);
+    },
+  })(req, res);
+}
