0178ee2837a23063aacea3881c4398d08813a948
nicolascasmuz <nicolascasmuz@hotmail.com>
2025-02-08 22:31:27 -0300

    commit-034

--- pages/api/order/[orderID].ts
+++ pages/api/order/[orderID].ts
@@ -1,12 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import methods from "micro-method-router";
 import { getOrderByID } from "lib/controllers/order";
+import Cors from "cors";
 
-export default methods({
-  async get(req: NextApiRequest, res: NextApiResponse) {
-    const { orderID } = req.query;
-
-    const order = await getOrderByID(orderID);
-    res.status(200).json(order);
-  },
+const cors = Cors({
+  methods: ["GET", "POST", "PATCH", "OPTIONS", "HEAD"],
 });
+
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function corsHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await runMiddleware(req, res, cors);
+
+  await methods({
+    async get(req: NextApiRequest, res: NextApiResponse) {
+      const { orderID } = req.query;
+
+      const order = await getOrderByID(orderID);
+      res.status(200).json(order);
+    },
+  })(req, res);
+}
